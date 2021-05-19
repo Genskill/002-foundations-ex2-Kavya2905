6193764c02028f80c1315f7c06e881a4c401ba83
Kavya2905 <74311514+Kavya2905@users.noreply.github.com>
2021-05-19 16:15:41 +0530

    Create pythagorean.c

---
+++ pythagorean.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main() {
+    int a,b,c;
+    
+    a = get_float("Enter number 1\n");
+    b = get_float("Enter number 2\n");
+    c = get_float("Enter number 3\n");
+    
+    if(a*a + b*b = c*c || b*b+c*c = a*a || a*a + c*c = b*b){
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    
+    return 0;
+}
