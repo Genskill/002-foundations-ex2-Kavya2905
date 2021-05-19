f48d1afc69bc398e2ed47219e802a093191379cd
Kavya2905 <74311514+Kavya2905@users.noreply.github.com>
2021-05-19 16:18:04 +0530

    Update pythagorean.c

--- pythagorean.c
+++ pythagorean.c
@@ -8,7 +8,7 @@ int main() {
     b = get_float("Enter number 2\n");
     c = get_float("Enter number 3\n");
     
-    if(a*a + b*b = c*c || b*b+c*c = a*a || a*a + c*c = b*b){
+    if(a*a + b*b == c*c || b*b+c*c == a*a || a*a + c*c == b*b){
         printf("Yes");
     }
     else
