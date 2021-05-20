65750c83ec2394d1b4003f2b2058ea7cde707d5b
Kavya2905 <74311514+Kavya2905@users.noreply.github.com>
2021-05-20 19:09:39 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -1,56 +1,36 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 #include <cs50.h>
-
-int main() {
-    
-    int p, n;
-    
-    p = get_int("Enter pattern number: ");
-
-    n = get_int("Enter number of lines: ");
-
-    printf("\n");
-    
-    if (p == 1)
+int main (void)
+{
+  int ch;
+  ch=get_int("Enter type of pattern (1 or 2) ");
+  int row;
+  int space;
+  if (ch==1)
+  {
+    row=get_int("Number of rows");
+    for (int i=row;i>=1;i--)
     {
-        for(int i = n; i >= 1; i--)
-        {        
-        // loop to print *
-        for(int j = i; j >= 1; j--)
-            {
-            printf("#");
-            }               
-
-        printf("\n");
+      for (int j=1;j<=i;j++)
+        printf ("#");
+      printf ("\n");
     }
-        
-    }
-        
-    
-    if (p==2)
+  }
+  else if (ch==2)
+  {
+    row=get_int("Number of rows");
+    space=1;
+    for (int i=row;i>=1;i--)
     {
-        for (int i = 1 ; i <= n; i++ ) 
-    {
-      for (int j = 1 ; j <= n; j++ ) 
-      {
-      	if(j <= n-i)
-      	{
-          printf(" ");
-	    }
-	    else
-	    {
-	    printf("#");		  	
-	    }         
-      }
-      printf("\n");
-    }
-        
-    }
-        
-    
-    
-    
-    
-    return 0;
+      for (int j=1;j<=i-1;j++)
+        printf (" ");
+      for (int k=1;k<=space;k++)
+        printf ("#");
+      printf ("\n");
+      space++;  
+     }
+   }             
+    else         
+    printf("Inavalid choice");
 }
